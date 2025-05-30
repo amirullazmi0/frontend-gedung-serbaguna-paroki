825ba97722a1c323d42d6791d090c0f0817a6bbe
Amirull Azmi <amirullazmi0@gmail.com>
2025-05-31 00:57:41 +0700

    feat: add FormLogin component for user authentication

---
+++ src/app/auth/login/components/FormLogin.tsx
@@ -0,0 +1,150 @@
+'use client';
+import React from 'react';
+import { Stack, TextField, Button, Typography } from '@mui/material';
+import { Controller, useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { loginSchema } from './loginConfig';
+import { colorPallete } from '@/app/utils/colorspallete';
+import { useRouter } from 'next/navigation';
+
+type LoginFormInputs = {
+	email: string;
+	password: string;
+};
+
+const FormLogin = () => {
+	const {
+		control,
+		handleSubmit,
+		formState: { errors, isValid },
+	} = useForm<LoginFormInputs>({
+		resolver: yupResolver(loginSchema),
+		mode: 'onChange',
+		defaultValues: {
+			email: '',
+			password: '',
+		},
+	});
+
+	const navigatiom = useRouter();
+
+	const onSubmit = (data: LoginFormInputs) => {
+		console.log('Form Data:', data);
+		// Handle login logic here
+	};
+
+	return (
+		<Stack
+			component='form'
+			onSubmit={handleSubmit(onSubmit)}
+			sx={{
+				padding: 7,
+				borderRadius: 8,
+				boxShadow: '0px 0px 20px -5px rgba(0, 0, 0, 0.25)',
+				bgcolor: colorPallete.white,
+				maxWidth: {
+					xs: '100%',
+					sm: 400,
+				},
+				minWidth: {
+					xs: '90%',
+					sm: 400,
+				},
+				margin: 'auto',
+				gap: 2,
+			}}>
+			<Typography
+				variant='h3'
+				fontWeight={600}
+				sx={{
+					color: colorPallete.primary,
+				}}>
+				LOGIN
+			</Typography>
+			<Controller
+				name='email'
+				control={control}
+				render={({ field }) => (
+					<TextField
+						{...field}
+						label='Email'
+						type='email'
+						error={!!errors.email}
+						helperText={errors.email?.message}
+						fullWidth
+					/>
+				)}
+			/>
+
+			<Controller
+				name='password'
+				control={control}
+				render={({ field }) => (
+					<TextField
+						{...field}
+						label='Password'
+						type='password'
+						error={!!errors.password}
+						helperText={errors.password?.message}
+						fullWidth
+					/>
+				)}
+			/>
+
+			<Button
+				type='submit'
+				variant='contained'
+				disabled={!isValid}
+				fullWidth>
+				Login
+			</Button>
+
+			<Button
+				variant='text'
+				sx={{ padding: 0, margin: 0, width: 'fit-content', textTransform: 'none' }}>
+				<Typography
+					variant='body2'
+					sx={{
+						textAlign: 'center',
+						color: colorPallete.primary,
+					}}>
+					Lupa Password?
+				</Typography>
+			</Button>
+			<hr style={{ margin: '20px 10px' }} />
+
+			<Stack
+				direction={'row'}
+				justifyContent={'center'}
+				alignItems={'center'}
+				gap={1}>
+				<Typography variant='body2'>Belum Punya Akun? </Typography>
+				<Button
+					variant='contained'
+					size='small'
+					color='warning'
+					onClick={() => navigatiom.push('/auth/register')}
+					disableElevation
+					sx={{
+						textTransform: 'none',
+					}}>
+					Register
+				</Button>
+			</Stack>
+
+			<Button
+				size='small'
+				color='success'
+				variant='contained'
+				onClick={() => navigatiom.push('/')}
+				disableElevation
+				sx={{
+					textTransform: 'none',
+				}}>
+				Kembali Ke Maps
+			</Button>
+		</Stack>
+	);
+};
+
+export default FormLogin;
