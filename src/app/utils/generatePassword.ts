3be21f7db4d2015edf60bd58be9c31e287c9c30a
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-01 01:28:03 +0700

    fix: remove special characters from password generation for simplicity

--- src/app/utils/generatePassword.ts
+++ src/app/utils/generatePassword.ts
@@ -2,9 +2,8 @@ export function generatePassword(length = 8) {
   const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   const lowercase = "abcdefghijklmnopqrstuvwxyz";
   const digits = "0123456789";
-  const special = "!@#$%^&*()-_=+[]{}|;:,.<>?";
 
-  const allChars = uppercase + lowercase + digits + special;
+  const allChars = uppercase + lowercase + digits;
 
   let password = "";
 
@@ -12,7 +11,6 @@ export function generatePassword(length = 8) {
   password += uppercase[Math.floor(Math.random() * uppercase.length)];
   password += lowercase[Math.floor(Math.random() * lowercase.length)];
   password += digits[Math.floor(Math.random() * digits.length)];
-  password += special[Math.floor(Math.random() * special.length)];
 
   // Fill remaining length with random chars from all categories
   for (let i = 4; i < length; i++) {
