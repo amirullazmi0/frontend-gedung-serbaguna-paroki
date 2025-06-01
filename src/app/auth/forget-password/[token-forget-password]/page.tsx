841e34962aa4af927a50129ba931f1efcbfd40f1
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-01 14:42:22 +0700

    feat: add forget password page with centered form layout

---
+++ src/app/auth/forget-password/[token-forget-password]/page.tsx
@@ -0,0 +1,14 @@
+import Stack from '@mui/material/Stack';
+import FormNewPassword from '../components/FormNewPassword';
+
+export default function Home() {
+	return (
+		<Stack
+			justifyContent={'center'}
+			alignItems={'center'}
+			height={'100%'}
+			py={4}>
+			<FormNewPassword />
+		</Stack>
+	);
+}
