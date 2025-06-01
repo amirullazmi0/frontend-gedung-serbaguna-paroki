7f32df5a825188358e4a00883f28656012f326e4
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-01 14:42:16 +0700

    feat: enhance forget password page layout with centered form

--- src/app/auth/forget-password/page.tsx
+++ src/app/auth/forget-password/page.tsx
@@ -1,5 +1,14 @@
 import Stack from '@mui/material/Stack';
+import FormForgetPassword from './components/FormForgetPassword';
 
 export default function Home() {
-	return <Stack></Stack>;
+	return (
+		<Stack
+			justifyContent={'center'}
+			alignItems={'center'}
+			height={'100%'}
+			py={4}>
+			<FormForgetPassword />
+		</Stack>
+	);
 }
