0911ba50a53e55cdf28490826612aac4cd6c0fee
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-09 22:12:06 +0700

    feat: implement alert handling for password reset form submission

--- src/app/auth/forget-password/components/FormForgetPassword.tsx
+++ src/app/auth/forget-password/components/FormForgetPassword.tsx
@@ -1,17 +1,22 @@
 'use client';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Stack, TextField, Button, Typography } from '@mui/material';
 import { Controller, useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { colorPallete } from '@/app/utils/colorspallete';
 import { useRouter } from 'next/navigation';
 import { InferType } from 'yup';
-import { forgetPasswordSchema } from './forgetPasswordConfig';
+import { forgetPasswordSchema } from '@/app/hook/auth/authConfig';
+import { Alert, AlertType } from '@/app/components/Alert/Alert';
+import { useForgetPassword } from '@/app/hook/auth/useAuthMutation';
+import { AxiosError } from 'axios';
+import { ErrorData } from '@/app/utils/globalsApiResponse';
 
 const FormForgetPassword = () => {
 	const {
 		control,
 		handleSubmit,
+		reset,
 		formState: { errors, isValid },
 	} = useForm<InferType<typeof forgetPasswordSchema>>({
 		resolver: yupResolver(forgetPasswordSchema),
@@ -22,11 +27,52 @@ const FormForgetPassword = () => {
 	});
 
 	const navigatiom = useRouter();
+	const [alertShow, setAlertShow] = useState(false);
+	const [alertMessage, setAlertMessage] = useState<React.ReactNode>('');
+	const [alertType, setAlertType] = useState<AlertType>('success');
+	const [showTime, setShowTime] = useState<number>(4000);
+	const { mutateAsync: forgetPassword, isPending, isSuccess, isError, error, data } = useForgetPassword();
 
-	const onSubmit = (data: InferType<typeof forgetPasswordSchema>) => {
-		console.log('Form Data:', data);
-		// Handle login logic here
+	const onSubmit = async (data: InferType<typeof forgetPasswordSchema>) => {
+		const formData = { ...data, email: data.email.toLowerCase() };
+		await forgetPassword(formData);
 	};
+
+	useEffect(() => {
+		if (isSuccess) {
+			if (data.success) {
+				setAlertType('success');
+				setAlertMessage(
+					<>
+						<Stack gap={1}>
+							<Typography textAlign={'center'}>Email Lupa Password Berhasil dikirim</Typography>
+							<Typography textAlign={'center'}>Silahkan cek email</Typography>
+						</Stack>
+					</>
+				);
+				setShowTime(10000);
+				setAlertShow(true);
+				setTimeout(() => {
+					navigatiom.push('/auth/login');
+				}, showTime - 1000);
+			} else {
+				setAlertType('error');
+				setAlertMessage(data.message);
+				setShowTime(4000);
+				setAlertShow(true);
+			}
+			reset();
+		}
+
+		if (isError) {
+			const err: AxiosError = error as AxiosError;
+			const errorData: ErrorData = err.response?.data as ErrorData;
+			setAlertType('error');
+			setAlertMessage(errorData.message || 'Login Failed');
+			setShowTime(4000);
+			setAlertShow(true);
+		}
+	}, [isSuccess, isError, error, reset, data]);
 
 	return (
 		<Stack
@@ -48,6 +94,13 @@ const FormForgetPassword = () => {
 				margin: 'auto',
 				gap: 2,
 			}}>
+			<Alert
+				type={alertType}
+				open={alertShow}
+				onClose={() => setAlertShow(false)}
+				message={alertMessage}
+				timeout={showTime}
+			/>
 			<Typography
 				variant='h5'
 				fontWeight={600}
@@ -75,6 +128,7 @@ const FormForgetPassword = () => {
 
 			<Button
 				type='submit'
+				loading={isPending}
 				variant='contained'
 				disabled={!isValid}
 				fullWidth>
@@ -82,25 +136,6 @@ const FormForgetPassword = () => {
 			</Button>
 
 			<hr style={{ margin: '20px 10px' }} />
-
-			<Stack
-				direction={'row'}
-				justifyContent={'center'}
-				alignItems={'center'}
-				gap={1}>
-				<Typography variant='body2'>Belum Punya Akun? </Typography>
-				<Button
-					variant='contained'
-					size='small'
-					color='warning'
-					onClick={() => navigatiom.push('/auth/register')}
-					disableElevation
-					sx={{
-						textTransform: 'none',
-					}}>
-					Register
-				</Button>
-			</Stack>
 
 			<Button
 				size='small'
