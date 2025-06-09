be9e9fea6d03f0102eedaaf5a0cc2c7e0068e26b
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-09 21:42:08 +0700

    feat: add activation request handling to useAuthMutation hook

--- src/app/hook/auth/useAuthMutation.ts
+++ src/app/hook/auth/useAuthMutation.ts
@@ -2,9 +2,9 @@ import { useMutation } from '@tanstack/react-query';
 import axios from 'axios';
 import { GlobalApiResponse } from '../../utils/globalsApiResponse';
 import { InferType } from 'yup';
-import { activationSchema, loginSchema, registerSchema } from './authConfig';
+import { activationRequestSchema, activationSchema, loginSchema, registerSchema } from './authConfig';
 import Cookies from "js-cookie";
-// Define the expected response type for login and register
+
 interface LoginResponse {
   accessToken: string;
   email: string;
@@ -26,29 +26,31 @@ interface activationResponse {
   email: string
 }
 
-// Axios instance for API requests
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL, // Replace with your actual API base URL
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-// Function to handle login API request
 const login = async (data: InferType<typeof loginSchema>): Promise<GlobalApiResponse<LoginResponse>> => {
   const response = await axiosInstance.post('/auth/login', data);
-  return response.data; // Return the response (e.g., token or user data)
+  return response.data;
 };
 
-// Function to handle register API request
 const register = async (data: InferType<typeof registerSchema>): Promise<GlobalApiResponse<RegisterResponse>> => {
   const response = await axiosInstance.post('/auth/register', data);
-  return response.data; // Return the response (e.g., token or user data)
+  return response.data;
 };
 
 const activation = async (data: InferType<typeof activationSchema>): Promise<GlobalApiResponse<activationResponse>> => {
-  const response = await axiosInstance.post('/auth/activation', data);  // The endpoint might be `/auth/activate`
-  return response.data;  // Assuming the response contains a message or success indicator
+  const response = await axiosInstance.post('/auth/activation', data);
+  return response.data;
+};
+
+const activationRequest = async (data: InferType<typeof activationRequestSchema>): Promise<GlobalApiResponse<activationResponse>> => {
+  const response = await axiosInstance.post('/auth/activation-token-request', data);
+  return response.data;
 };
 
 // Hook to handle login mutation
@@ -59,7 +61,6 @@ export const useLogin = () => {
       Cookies.set('access-token', data.data?.accessToken ?? '', { expires: 7 });
     },
     onError: (error) => {
-      // Handle error (e.g., show an error message)
       console.error('Login failed:', error);
     },
   });
@@ -70,11 +71,9 @@ export const useRegister = () => {
   return useMutation<GlobalApiResponse<RegisterResponse>, Error, InferType<typeof registerSchema>>({
     mutationFn: register,
     onSuccess: (data) => {
-      // Handle success (e.g., show success message or redirect to login)
       console.log('Registration successful:', data);
     },
     onError: (error) => {
-      // Handle error (e.g., show an error message)
       console.error('Registration failed:', error);
     },
   });
@@ -85,11 +84,21 @@ export const useActivation = () => {
     mutationFn: activation,
     onSuccess: (data) => {
       console.log('Activation successful:', data);
-      // You might want to handle success here, such as redirecting the user or showing a success message.
     },
     onError: (error) => {
       console.error('Activation failed:', error);
-      // Handle error (e.g., show an error message)
+    },
+  });
+};
+
+export const useActivationRequest = () => {
+  return useMutation<GlobalApiResponse<activationResponse>, Error, InferType<typeof activationRequestSchema>>({
+    mutationFn: activationRequest,
+    onSuccess: (data) => {
+      console.log('Activation request successful:', data);
+    },
+    onError: (error) => {
+      console.error('Activation request failed:', error);
     },
   });
 };
