d75e73e6e50d2f147d33cbb4136b454dd36d75b4
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-09 16:27:06 +0700

    refactor: remove unused login and registration configuration and hooks

--- src/app/auth/login/components/loginConfig.ts
+++
@@ -1,7 +0,0 @@
-import * as yup from "yup";
-
-export const loginSchema = yup.object({
-  email: yup.string().required('Email wajib diisi'),
-  password: yup.string().required('Password wajib diisi'),
-});
-
