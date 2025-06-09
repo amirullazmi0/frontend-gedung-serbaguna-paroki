317193e626b7a26d68d3d581041c0f540ca45919
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-09 15:15:55 +0700

    feat: update next.config.ts to include environment variables configuration

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     domains: ['listingsprod.blob.core.windows.net']
+  },
+  env: {
+    API_URL: process.env.NEXT_PUBLIC_API_URL
   }
 };
 
