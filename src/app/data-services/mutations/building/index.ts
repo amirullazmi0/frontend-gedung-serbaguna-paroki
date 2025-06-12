a056cc818111874a13e84ec6b3ace1a284cdb0df
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-12 16:07:57 +0700

    feat: add 'post-building-photo' mutation for building image uploads

--- src/app/data-services/mutations/building/index.ts
+++ src/app/data-services/mutations/building/index.ts
@@ -1,12 +1,16 @@
 import { MutationDataService } from "../../types";
 
 export type BuildingMutationKeys =
-  | 'post-building'
+  | 'post-building' | 'post-building-photo'
 
 export const buildingMutations: MutationDataService<BuildingMutationKeys> = {
   'post-building': {
     url: 'member',
     method: 'POST',
     refetchQueries: ['get-building'],
+  },
+  'post-building-photo': {
+    url: 'building/image',
+    method: 'POST',
   },
 };
