23615f3a8da09882f9efa7cfc099c591475e0dc3
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-16 01:10:20 +0700

    feat: refactor BuildingItemType and related interfaces for improved structure

--- src/app/DTO/building.ts
+++ src/app/DTO/building.ts
@@ -1,91 +1,33 @@
-export interface buildingItemType {
-  id: number;
-  name: string;
-  latitude: number;
-  longitude: number;
-  description: string;
-  image: string[];
+export interface addressType {
+  jalan: string;
+  rt: string;
+  rw: string;
+  kelurahan: string;
+  kecamatan: string;
+  lat: string;
+  lng: string;
+  kota: number;
+  provinsi: string;
+  kodepos: string;
 }
-export const buildingsDummy: buildingItemType[] = [
-  {
-    id: 1,
-    name: 'Gedung Sudirman',
-    latitude: -6.2248,
-    longitude: 106.8102,
-    description: 'Gedung perkantoran di kawasan Sudirman Jakarta.',
-    image: ['https://listingsprod.blob.core.windows.net/ourlistings-idn/29ba8826-d7e0-41e4-9487-61732e89f31f/93d1c04b-c7d0-46b8-8342-e937d2ab51e7-w']
-  },
-  {
-    id: 2,
-    name: 'Gedung Thamrin',
-    latitude: -6.1935,
-    longitude: 106.8234,
-    description: 'Gedung perkantoran di kawasan Thamrin Jakarta.',
-    image: ['https://listingsprod.blob.core.windows.net/ourlistings-idn/29ba8826-d7e0-41e4-9487-61732e89f31f/93d1c04b-c7d0-46b8-8342-e937d2ab51e7-w']
-  },
-  {
-    id: 3,
-    name: 'Gedung Kuningan',
-    latitude: -6.2254,
-    longitude: 106.8290,
-    description: 'Gedung perkantoran di kawasan Kuningan Jakarta Selatan.',
-    image: ['https://listingsprod.blob.core.windows.net/ourlistings-idn/29ba8826-d7e0-41e4-9487-61732e89f31f/93d1c04b-c7d0-46b8-8342-e937d2ab51e7-w']
-  },
-  {
-    id: 4,
-    name: 'Gedung Rasuna Said',
-    latitude: -6.2171,
-    longitude: 106.8316,
-    description: 'Gedung perkantoran dan komersial di Jalan Rasuna Said.',
-    image: ['https://listingsprod.blob.core.windows.net/ourlistings-idn/29ba8826-d7e0-41e4-9487-61732e89f31f/93d1c04b-c7d0-46b8-8342-e937d2ab51e7-w']
-  },
-  {
-    id: 5,
-    name: 'Gedung Balai Sudirman',
-    latitude: -6.2245,
-    longitude: 106.8057,
-    description: 'Gedung pertemuan dan kantor di Jalan Sudirman.',
-    image: ['https://listingsprod.blob.core.windows.net/ourlistings-idn/29ba8826-d7e0-41e4-9487-61732e89f31f/93d1c04b-c7d0-46b8-8342-e937d2ab51e7-w']
-  },
-  {
-    id: 6,
-    name: 'Gedung Wisma 46',
-    latitude: -6.2243,
-    longitude: 106.8097,
-    description: 'Gedung pencakar langit ikonik di pusat bisnis Jakarta.',
-    image: ['https://listingsprod.blob.core.windows.net/ourlistings-idn/29ba8826-d7e0-41e4-9487-61732e89f31f/93d1c04b-c7d0-46b8-8342-e937d2ab51e7-w']
-  },
-  {
-    id: 7,
-    name: 'Gedung Bank Indonesia',
-    latitude: -6.1751,
-    longitude: 106.8272,
-    description: 'Gedung pusat Bank Indonesia di Jakarta Pusat.',
-    image: ['https://listingsprod.blob.core.windows.net/ourlistings-idn/29ba8826-d7e0-41e4-9487-61732e89f31f/93d1c04b-c7d0-46b8-8342-e937d2ab51e7-w']
-  },
-  {
-    id: 8,
-    name: 'Gedung Plaza Indonesia',
-    latitude: -6.1931,
-    longitude: 106.8217,
-    description: 'Pusat perbelanjaan dan kantor di kawasan Thamrin.',
-    image: ['https://listingsprod.blob.core.windows.net/ourlistings-idn/29ba8826-d7e0-41e4-9487-61732e89f31f/93d1c04b-c7d0-46b8-8342-e937d2ab51e7-w']
-  },
-  {
-    id: 9,
-    name: 'Gedung Kota Kasablanka',
-    latitude: -6.2241,
-    longitude: 106.8361,
-    description: 'Gedung multifungsi di kawasan Casablanca Jakarta Selatan.',
-    image: ['https://listingsprod.blob.core.windows.net/ourlistings-idn/29ba8826-d7e0-41e4-9487-61732e89f31f/93d1c04b-c7d0-46b8-8342-e937d2ab51e7-w']
-  },
-  {
-    id: 10,
-    name: 'Gedung Pacific Place',
-    latitude: -6.2196,
-    longitude: 106.8232,
-    description: 'Gedung perkantoran dan pusat perbelanjaan di SCBD.',
-    image: ['https://listingsprod.blob.core.windows.net/ourlistings-idn/29ba8826-d7e0-41e4-9487-61732e89f31f/93d1c04b-c7d0-46b8-8342-e937d2ab51e7-w']
-  },
-];
+
+export interface supportDocumentType {
+  id: string;
+  name: string;
+  templateDocumentUrl: string;
+}
+export interface BuildingPhotoType {
+  id: string;
+  url: string;
+}
+
+export interface BuildingItemType {
+  id: string;
+  name: string;
+  price: number;
+  description: string;
+  buildingPhoto: BuildingPhotoType[];
+  buildingAddress: addressType[];
+  supportDocumentRequirement: supportDocumentType[];
+}
 
