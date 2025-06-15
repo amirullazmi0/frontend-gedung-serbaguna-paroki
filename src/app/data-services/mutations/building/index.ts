ec795f59e6c0f07b0d8bac4b9b9337b5adabaf18
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-16 01:10:12 +0700

    feat: add 'post-building-document' mutation and update URL for 'post-building'

--- src/app/data-services/mutations/building/index.ts
+++ src/app/data-services/mutations/building/index.ts
@@ -1,16 +1,20 @@
 import { MutationDataService } from "../../types";
 
 export type BuildingMutationKeys =
-  | 'post-building' | 'post-building-photo'
+  | 'post-building' | 'post-building-photo' | 'post-building-document';
 
 export const buildingMutations: MutationDataService<BuildingMutationKeys> = {
   'post-building': {
-    url: 'member',
+    url: 'building',
     method: 'POST',
     refetchQueries: ['get-building'],
   },
   'post-building-photo': {
     url: 'building/image',
+    method: 'POST',
+  },
+  'post-building-document': {
+    url: 'building/document',
     method: 'POST',
   },
 };
