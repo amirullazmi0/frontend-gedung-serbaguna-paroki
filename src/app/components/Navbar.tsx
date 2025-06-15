a0097f9612b13680ad27351b6bae97f8042f12bd
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-16 01:10:05 +0700

    feat: refactor Navbar component to improve structure and integrate logo with user authentication handling

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,43 +1,32 @@
 'use client';
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
+import React, { useContext, useState } from 'react';
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
 import { colorPallete } from '../utils/colorspallete';
-import { Stack } from '@mui/material';
-import { usePathname } from 'next/navigation';
+import { Button, Stack } from '@mui/material';
+import { usePathname, useRouter } from 'next/navigation';
+import { authContext } from '../provider/auth-provider/authProvider';
 
-const pages = ['Products', 'Pricing', 'Blog'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+import logo from '@/assets/logo.jpg';
+import Image from 'next/image';
 
 function Navbar() {
-	const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
-	const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
-
-	const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-		setAnchorElNav(event.currentTarget);
-	};
+	const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
+	const route = useRouter();
 	const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
 		setAnchorElUser(event.currentTarget);
-	};
-
-	const handleCloseNavMenu = () => {
-		setAnchorElNav(null);
 	};
 
 	const handleCloseUserMenu = () => {
 		setAnchorElUser(null);
 	};
+
+	const { authenticated, user } = useContext(authContext);
 
 	const pathName = usePathname();
 
@@ -61,45 +50,64 @@ function Navbar() {
 					borderBottomRightRadius: 10,
 					borderBottomLeftRadius: 10,
 				}}>
-				<Stack>
-					<Typography variant='h5'>Logo</Typography>
+				<Stack
+					direction={'row'}
+					gap={1}>
+					<Image
+						alt='logo'
+						src={logo}
+						width={50}
+						height={50}
+					/>
+					<Typography
+						fontWeight={'600'}
+						sx={{
+							color: colorPallete['primary'],
+						}}>
+						Sewa Gedung <br />
+						Sekarang
+					</Typography>
 				</Stack>
 				<Stack>
-					<Box sx={{ flexGrow: 0 }}>
-						<Tooltip title='Open settings'>
-							<IconButton
-								onClick={handleOpenUserMenu}
-								sx={{ p: 0 }}>
-								<Avatar
-									alt='Remy Sharp'
-									src='/static/images/avatar/2.jpg'
-								/>
-							</IconButton>
-						</Tooltip>
-						<Menu
-							sx={{ mt: '45px' }}
-							id='menu-appbar'
-							anchorEl={anchorElUser}
-							anchorOrigin={{
-								vertical: 'top',
-								horizontal: 'right',
-							}}
-							keepMounted
-							transformOrigin={{
-								vertical: 'top',
-								horizontal: 'right',
-							}}
-							open={Boolean(anchorElUser)}
-							onClose={handleCloseUserMenu}>
-							{settings.map(setting => (
-								<MenuItem
-									key={setting}
-									onClick={handleCloseUserMenu}>
-									<Typography sx={{ textAlign: 'center' }}>{setting}</Typography>
-								</MenuItem>
-							))}
-						</Menu>
-					</Box>
+					{authenticated ? (
+						<>
+							<Box sx={{ flexGrow: 0 }}>
+								<Tooltip title='Open settings'>
+									<IconButton
+										onClick={handleOpenUserMenu}
+										sx={{ p: 0 }}>
+										<Avatar alt={user?.email ?? 'account'} />
+									</IconButton>
+								</Tooltip>
+								<Menu
+									sx={{ mt: '45px' }}
+									// id='menu-appbar'
+									anchorEl={anchorElUser}
+									open={Boolean(anchorElUser)}
+									onClose={handleCloseUserMenu}>
+									{user && (
+										<MenuItem onClick={handleCloseUserMenu}>
+											<Typography sx={{ textAlign: 'center' }}>Dashboard</Typography>
+										</MenuItem>
+									)}
+
+									{user && (
+										<MenuItem onClick={handleCloseUserMenu}>
+											<Typography sx={{ textAlign: 'center' }}>Logout</Typography>
+										</MenuItem>
+									)}
+								</Menu>
+							</Box>
+						</>
+					) : (
+						<>
+							<Button
+								variant='contained'
+								onClick={() => route.push('/auth/login')}>
+								Login
+							</Button>
+						</>
+					)}
 				</Stack>
 			</Stack>
 		);
