b762468de86b1ca972a964ca5be99326703475d8
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-16 01:09:47 +0700

    feat: add BuildingTable component for displaying building data with responsive layout

---
+++ src/app/(user-auth)/admin/components/BuildingTable.tsx
@@ -0,0 +1,92 @@
+'use client';
+import { BuildingItemType } from '@/app/DTO/building';
+import useQueryApiRequest from '@/app/hook/useQueryApiRequest';
+import { GlobalApiResponse } from '@/app/utils/globalsApiResponse';
+import { Card, CardActions, CardContent, Divider, IconButton, Stack, Typography, useMediaQuery, useTheme } from '@mui/material';
+import Image from 'next/image';
+import React from 'react';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import SourceIcon from '@mui/icons-material/Source';
+import { formatRupiah } from '@/app/utils/formatCurency';
+
+const BuildingTable = () => {
+	const { data, isLoading } = useQueryApiRequest<GlobalApiResponse<BuildingItemType[]>>({
+		key: 'get-admin-building',
+		withAuth: true,
+	});
+
+	const theme = useTheme();
+	const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
+	if (isLoading) {
+		return <Typography textAlign='center'>Loading...</Typography>;
+	}
+
+	const renderAddress = (address?: BuildingItemType['buildingAddress'][0]) => {
+		if (!address) return '-';
+		return `${address.jalan}, ${address.rt}/${address.rw}, ${address.kelurahan}, ${address.kecamatan}, ${address.kota}, ${address.provinsi} ${address.kodepos}`;
+	};
+
+	return (
+		<Stack
+			mt={5}
+			px={{ xs: 2, md: 5 }}>
+			<Stack
+				display='grid'
+				gap={2}
+				sx={{
+					gridTemplateColumns: {
+						xs: 'repeat(1, 1fr)', // 1 column on mobile
+						md: 'repeat(3, 1fr)', // 3 columns on medium screens
+						lg: 'repeat(4, 1fr)', // 5 columns on large screens
+					},
+				}}>
+				{data?.data?.map((row, index) => (
+					<Card
+						key={index}
+						sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+						<CardContent>
+							<Typography
+								variant='subtitle2'
+								color='textSecondary'>
+								Gedung #{index + 1}
+							</Typography>
+							<Typography variant='h6'>{row.name}</Typography>
+							<Typography>{formatRupiah(row.price)}</Typography>
+							<Typography
+								variant='body2'
+								mt={1}>
+								{renderAddress(row.buildingAddress?.[0])}
+							</Typography>
+
+							{row.buildingPhoto?.[0]?.url && (
+								<Image
+									alt={`${row.name} photo`}
+									src={row.buildingPhoto[0].url}
+									width={400}
+									height={200}
+									style={{ objectFit: 'cover', borderRadius: 8, marginTop: 10, width: '100%' }}
+								/>
+							)}
+						</CardContent>
+						<Divider />
+						<CardActions sx={{ justifyContent: 'center', flexWrap: isMobile ? 'wrap' : 'nowrap' }}>
+							<IconButton color='success'>
+								<SourceIcon />
+							</IconButton>
+							<IconButton color='warning'>
+								<EditIcon />
+							</IconButton>
+							<IconButton color='error'>
+								<DeleteIcon />
+							</IconButton>
+						</CardActions>
+					</Card>
+				))}
+			</Stack>
+		</Stack>
+	);
+};
+
+export default BuildingTable;
