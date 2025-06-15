ea09fa016e47082aabd0e78f26713d002aa2d52d
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-16 01:09:35 +0700

    feat: refactor FormAddBuilding to use FormProvider and improve form handling

--- src/app/(user-auth)/admin/building/add/components/FormAddBuilding.tsx
+++ src/app/(user-auth)/admin/building/add/components/FormAddBuilding.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 import { Stack, TextField, Button, Typography, InputAdornment, IconButton, useTheme, useMediaQuery } from '@mui/material';
-import { Controller, useForm } from 'react-hook-form';
+import { Controller, FormProvider, useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { colorPallete } from '@/app/utils/colorspallete';
 import { useRouter } from 'next/navigation';
@@ -9,13 +9,18 @@ import { InferType } from 'yup';
 import { Alert, AlertType } from '@/app/components/Alert/Alert';
 import { useLogin } from '@/app/hook/auth/useAuthMutation';
 import { AxiosError } from 'axios';
-import { ErrorData } from '@/app/utils/globalsApiResponse';
+import { ErrorData, GlobalApiResponse } from '@/app/utils/globalsApiResponse';
 import { AddItemBuildingRequestSchema } from '../../buildingConfig';
 import SelectLatLng from './SelectLatLng';
-import FormPhoto from '@/app/components/form-photo/FormPhoto';
 import FormSelectPhoto from './FormSelectPhoto';
+import FormSupportDocument from './FormSupportDocument';
+import useMutationApiRequest from '@/app/hook/useMutationApiRequest';
 
 const FormAddBuilding = () => {
+	const form = useForm<InferType<typeof AddItemBuildingRequestSchema>>({
+		resolver: yupResolver(AddItemBuildingRequestSchema),
+		mode: 'onChange',
+	});
 	const {
 		control,
 		handleSubmit,
@@ -24,16 +29,23 @@ const FormAddBuilding = () => {
 		trigger,
 		watch,
 		formState: { errors, isValid },
-	} = useForm<InferType<typeof AddItemBuildingRequestSchema>>({
-		resolver: yupResolver(AddItemBuildingRequestSchema),
-		mode: 'onChange',
-	});
+	} = form;
 
 	const [alertShow, setAlertShow] = useState(false);
 	const [alertMessage, setAlertMessage] = useState<React.ReactNode>('');
 	const [alertType, setAlertType] = useState<AlertType>('success');
 	const [showTime, setShowTime] = useState<number>(4000);
-	const { mutateAsync: login, isPending, isSuccess, isError, error, data } = useLogin();
+	const {
+		mutateAsync: postBuilding,
+		isPending,
+		isSuccess,
+		isError: isErrorPost,
+		error: errorPost,
+		data,
+	} = useMutationApiRequest<GlobalApiResponse<any>, InferType<typeof AddItemBuildingRequestSchema>>({
+		key: 'post-building',
+		authRequired: true,
+	});
 
 	const navigation = useRouter();
 
@@ -42,61 +54,70 @@ const FormAddBuilding = () => {
 	const isDesktop = useMediaQuery(theme.breakpoints.up('lg')) && !isMobile;
 
 	useEffect(() => {
-		console.log(watch('photo'));
-	}, [watch('photo')]);
-
-	useEffect(() => {
 		if (isSuccess) {
-			if (data.success) {
-				setAlertType('success');
-				setAlertMessage('Login Berhasil');
-				setShowTime(4000);
-				setAlertShow(true);
-				setTimeout(() => {
-					navigation.push('/');
-				}, showTime - 1000);
-			} else {
-				if (data.message == 'user not active') {
-					setAlertType('error');
-					setAlertMessage(
-						<Stack
-							gap={1}
-							justifyContent={'center'}
-							alignItems={'center'}>
-							<Typography>Akun anda belum aktif</Typography>
-							<Button
-								variant='contained'
-								onClick={() => navigation.push('/auth/activation-request')}
-								sx={{
-									backgroundColor: colorPallete.primary,
-									width: 'fit-content',
-								}}>
-								Aktivasi Sekarang
-							</Button>
-						</Stack>
-					);
-					setShowTime(20000);
-					setAlertShow(true);
-				}
-			}
-			reset();
+			setAlertType('success');
+			setAlertMessage('Login Berhasil');
+			setShowTime(4000);
+			setAlertShow(true);
+			setTimeout(() => {
+				navigation.push('/admin');
+			}, showTime - 1000);
 		}
 
-		if (isError) {
-			const err: AxiosError = error as AxiosError;
+		if (isErrorPost) {
+			const err: AxiosError = errorPost as AxiosError;
 			const errorData: ErrorData = err.response?.data as ErrorData;
 			setAlertType('error');
 			setAlertMessage(errorData.message || 'Login Failed');
 			setShowTime(4000);
 			setAlertShow(true);
 		}
-	}, [isSuccess, isError, error, reset, data]);
+	}, [isSuccess, isErrorPost, errorPost, reset, data]);
 
-	const onSubmit = async (data: InferType<typeof AddItemBuildingRequestSchema>) => {
-		const formData = { ...data };
-		// await login(formData);
+	useEffect(() => {
+		console.log('err', errors);
+	}, [errors]);
+
+	const buildFormData = (data: Record<string, any>): FormData => {
+		const formData = new FormData();
+
+		Object.entries(data).forEach(([key, value]) => {
+			if (Array.isArray(value)) {
+				value.forEach((item, index) => {
+					if (typeof item === 'object' && item !== null) {
+						Object.entries(item).forEach(([nestedKey, nestedValue]) => {
+							const formKey = `${key}[${index}][${nestedKey}]`;
+							if (typeof nestedValue === 'string' || nestedValue instanceof Blob) {
+								formData.append(formKey, nestedValue);
+							} else {
+								formData.append(formKey, JSON.stringify(nestedValue));
+							}
+						});
+					}
+				});
+			} else if (typeof value === 'object' && value !== null) {
+				Object.entries(value).forEach(([nestedKey, nestedValue]) => {
+					const formKey = `${key}[${nestedKey}]`;
+					if (typeof nestedValue === 'string' || nestedValue instanceof Blob) {
+						formData.append(formKey, nestedValue);
+					} else {
+						formData.append(formKey, JSON.stringify(nestedValue));
+					}
+				});
+			} else if (typeof value === 'string' || value instanceof Blob) {
+				formData.append(key, value);
+			} else {
+				formData.append(key, JSON.stringify(value));
+			}
+		});
+
+		return formData;
 	};
 
+	const onSubmit = async (data: InferType<typeof AddItemBuildingRequestSchema>) => {
+		console.log('data upload', data);
+		await postBuilding(data);
+	};
 	return (
 		<Stack
 			component='form'
@@ -122,255 +143,255 @@ const FormAddBuilding = () => {
 					TAMBAH GEDUNG
 				</Typography>
 			</Stack>
-			<Stack
-				gap={2}
-				sx={{
-					display: 'grid',
-					gridTemplateColumns: {
-						xs: 'repeat(1, 1fr)',
-						md: 'repeat(2, 1fr)',
-						lg: 'repeat(2, 1fr)',
-					},
-				}}>
-				{isDesktop && (
-					<Stack
-						padding={2}
-						gap={1}
-						height={'fit-content'}
-						bgcolor={colorPallete['low-grey']}
-						borderRadius={2}>
-						<Typography>Pilih Lokasi Gedung</Typography>
-						<SelectLatLng
-							onChange={e => {
-								console.log('onChange', e);
-								setValue('address.lat', e.lat.toString());
-								setValue('address.lng', e.lng.toString());
-								trigger(['address.lat', 'address.lng']);
-							}}
-						/>
-					</Stack>
-				)}
+			<FormProvider {...form}>
 				<Stack
 					gap={2}
 					sx={{
-						width: '100%',
-						maxWidth: {
-							xs: '100%',
-							md: '600px',
+						display: 'grid',
+						gridTemplateColumns: {
+							xs: 'repeat(1, 1fr)',
+							md: 'repeat(2, 1fr)',
+							lg: 'repeat(2, 1fr)',
 						},
 					}}>
-					<Controller
-						name='name'
-						control={control}
-						render={({ field }) => (
-							<TextField
-								{...field}
-								label='Nama'
-								variant='outlined'
-								error={!!errors.name}
-								helperText={errors.name?.message}
+					{isDesktop && (
+						<Stack
+							padding={2}
+							gap={1}
+							height={'fit-content'}
+							bgcolor={colorPallete['low-grey']}
+							borderRadius={2}>
+							<Typography>Pilih Lokasi Gedung</Typography>
+							<SelectLatLng
+								onChange={e => {
+									console.log('onChange', e);
+									setValue('address.lat', e.lat.toString());
+									setValue('address.lng', e.lng.toString());
+									trigger(['address.lat', 'address.lng']);
+								}}
 							/>
-						)}
-					/>
-
-					{/* Price Field */}
-					<Controller
-						name='price'
-						control={control}
-						render={({ field }) => (
-							<TextField
-								{...field}
-								label='Harga'
-								variant='outlined'
-								type='number'
-								error={!!errors.price}
-								helperText={errors.price?.message}
-							/>
-						)}
-					/>
-
-					{/* Description Field */}
-					<Controller
-						name='description'
-						control={control}
-						render={({ field }) => (
-							<TextField
-								{...field}
-								label='Deskripsi'
-								variant='outlined'
-								multiline
-								rows={4}
-								error={!!errors.description}
-								helperText={errors.description?.message}
-							/>
-						)}
-					/>
-
+						</Stack>
+					)}
 					<Stack
 						gap={2}
 						sx={{
-							display: 'grid',
-							gridTemplateColumns: {
-								xs: 'repeat(1, 1fr)',
-								md: 'repeat(2, 1fr)',
+							width: '100%',
+							maxWidth: {
+								xs: '100%',
+								md: '600px',
 							},
 						}}>
 						<Controller
-							name='address.jalan'
+							name='name'
 							control={control}
 							render={({ field }) => (
 								<TextField
 									{...field}
-									label='Jalan'
+									label='Nama Gedung'
 									variant='outlined'
-									error={!!errors.address?.jalan}
-									helperText={errors.address?.jalan?.message}
+									error={!!errors.name}
+									helperText={errors.name?.message}
 								/>
 							)}
 						/>
+
+						{/* Price Field */}
 						<Controller
-							name='address.rt'
+							name='price'
 							control={control}
 							render={({ field }) => (
 								<TextField
 									{...field}
-									label='RT'
+									label='Harga'
 									variant='outlined'
-									error={!!errors.address?.rt}
-									helperText={errors.address?.rt?.message}
+									type='number'
+									InputProps={{
+										startAdornment: <InputAdornment position='start'>Rp</InputAdornment>,
+									}}
+									error={!!errors.price}
+									helperText={errors.price?.message}
 								/>
 							)}
 						/>
+
+						{/* Description Field */}
 						<Controller
-							name='address.rw'
+							name='description'
 							control={control}
 							render={({ field }) => (
 								<TextField
 									{...field}
-									label='RW'
+									label='Deskripsi'
 									variant='outlined'
-									error={!!errors.address?.rw}
-									helperText={errors.address?.rw?.message}
+									multiline
+									rows={4}
+									error={!!errors.description}
+									helperText={errors.description?.message}
 								/>
 							)}
 						/>
-						<Controller
-							name='address.kelurahan'
-							control={control}
-							render={({ field }) => (
-								<TextField
-									{...field}
-									label='Kelurahan'
-									variant='outlined'
-									error={!!errors.address?.kelurahan}
-									helperText={errors.address?.kelurahan?.message}
-								/>
-							)}
-						/>
-						<Controller
-							name='address.kecamatan'
-							control={control}
-							render={({ field }) => (
-								<TextField
-									{...field}
-									label='Kecamatan'
-									variant='outlined'
-									error={!!errors.address?.kecamatan}
-									helperText={errors.address?.kecamatan?.message}
-								/>
-							)}
-						/>
-						<Controller
-							name='address.kota'
-							control={control}
-							render={({ field }) => (
-								<TextField
-									{...field}
-									label='Kota'
-									variant='outlined'
-									error={!!errors.address?.kota}
-									helperText={errors.address?.kota?.message}
-								/>
-							)}
-						/>
-						<Controller
-							name='address.provinsi'
-							control={control}
-							render={({ field }) => (
-								<TextField
-									{...field}
-									label='Provinsi'
-									variant='outlined'
-									error={!!errors.address?.provinsi}
-									helperText={errors.address?.provinsi?.message}
-								/>
-							)}
-						/>
-						<Controller
-							name='address.kodepos'
-							control={control}
-							render={({ field }) => (
-								<TextField
-									{...field}
-									label='Kodepos'
-									variant='outlined'
-									error={!!errors.address?.kodepos}
-									helperText={errors.address?.kodepos?.message}
-								/>
-							)}
-						/>
-						<Controller
-							name='address.lat'
-							control={control}
-							render={({ field }) => (
-								<TextField
-									{...field}
-									label='lat'
-									defaultValue={'0'}
-									disabled
-									variant='outlined'
-									error={!!errors.address?.lat}
-									helperText={errors.address?.lat?.message}
-								/>
-							)}
-						/>
-						<Controller
-							name='address.lng'
-							control={control}
-							render={({ field }) => (
-								<TextField
-									{...field}
-									label='lng'
-									disabled
-									defaultValue={'0'}
-									variant='outlined'
-									error={!!errors.address?.lng}
-									helperText={errors.address?.lng?.message}
-								/>
-							)}
-						/>
+
+						<Stack
+							gap={2}
+							sx={{
+								display: 'grid',
+								gridTemplateColumns: {
+									xs: 'repeat(1, 1fr)',
+									md: 'repeat(2, 1fr)',
+								},
+							}}>
+							<Controller
+								name='address.jalan'
+								control={control}
+								render={({ field }) => (
+									<TextField
+										{...field}
+										label='Jalan'
+										variant='outlined'
+										error={!!errors.address?.jalan}
+										helperText={errors.address?.jalan?.message}
+									/>
+								)}
+							/>
+							<Controller
+								name='address.rt'
+								control={control}
+								render={({ field }) => (
+									<TextField
+										{...field}
+										label='RT'
+										variant='outlined'
+										error={!!errors.address?.rt}
+										helperText={errors.address?.rt?.message}
+									/>
+								)}
+							/>
+							<Controller
+								name='address.rw'
+								control={control}
+								render={({ field }) => (
+									<TextField
+										{...field}
+										label='RW'
+										variant='outlined'
+										error={!!errors.address?.rw}
+										helperText={errors.address?.rw?.message}
+									/>
+								)}
+							/>
+							<Controller
+								name='address.kelurahan'
+								control={control}
+								render={({ field }) => (
+									<TextField
+										{...field}
+										label='Kelurahan'
+										variant='outlined'
+										error={!!errors.address?.kelurahan}
+										helperText={errors.address?.kelurahan?.message}
+									/>
+								)}
+							/>
+							<Controller
+								name='address.kecamatan'
+								control={control}
+								render={({ field }) => (
+									<TextField
+										{...field}
+										label='Kecamatan'
+										variant='outlined'
+										error={!!errors.address?.kecamatan}
+										helperText={errors.address?.kecamatan?.message}
+									/>
+								)}
+							/>
+							<Controller
+								name='address.kota'
+								control={control}
+								render={({ field }) => (
+									<TextField
+										{...field}
+										label='Kota'
+										variant='outlined'
+										error={!!errors.address?.kota}
+										helperText={errors.address?.kota?.message}
+									/>
+								)}
+							/>
+							<Controller
+								name='address.provinsi'
+								control={control}
+								render={({ field }) => (
+									<TextField
+										{...field}
+										label='Provinsi'
+										variant='outlined'
+										error={!!errors.address?.provinsi}
+										helperText={errors.address?.provinsi?.message}
+									/>
+								)}
+							/>
+							<Controller
+								name='address.kodepos'
+								control={control}
+								render={({ field }) => (
+									<TextField
+										{...field}
+										label='Kodepos'
+										type='number'
+										variant='outlined'
+										error={!!errors.address?.kodepos}
+										helperText={errors.address?.kodepos?.message}
+									/>
+								)}
+							/>
+							<Controller
+								name='address.lat'
+								control={control}
+								render={({ field }) => (
+									<TextField
+										{...field}
+										label='lat'
+										defaultValue={'0'}
+										disabled
+										variant='outlined'
+										error={!!errors.address?.lat}
+										helperText={errors.address?.lat?.message}
+									/>
+								)}
+							/>
+							<Controller
+								name='address.lng'
+								control={control}
+								render={({ field }) => (
+									<TextField
+										{...field}
+										label='lng'
+										disabled
+										defaultValue={'0'}
+										variant='outlined'
+										error={!!errors.address?.lng}
+										helperText={errors.address?.lng?.message}
+									/>
+								)}
+							/>
+						</Stack>
+
+						<FormSelectPhoto />
+						<FormSupportDocument />
+
+						{/* Submit Button */}
+						<Button
+							type='submit'
+							variant='contained'
+							color='primary'
+							// disabled={!isValid}
+							sx={{ mt: 2 }}>
+							Submit
+						</Button>
 					</Stack>
-
-					{/* Photo Field */}
-
-					<FormSelectPhoto
-						onChange={e => {
-							setValue('photo', e);
-							trigger('photo');
-						}}
-					/>
-
-					{/* Submit Button */}
-					<Button
-						type='submit'
-						variant='contained'
-						color='primary'
-						disabled={!isValid}
-						sx={{ mt: 2 }}>
-						Submit
-					</Button>
 				</Stack>
-			</Stack>
+			</FormProvider>
 		</Stack>
 	);
 };
