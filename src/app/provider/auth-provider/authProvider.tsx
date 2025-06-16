20a420eb0870ae619308361e8765ca6ad176d4a4
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-16 13:34:36 +0700

    fix: add missing useRouter import and ensure router refresh on logout

--- src/app/provider/auth-provider/authProvider.tsx
+++ src/app/provider/auth-provider/authProvider.tsx
@@ -5,6 +5,7 @@ import Cookies from 'js-cookie';
 import axios from 'axios';
 import { GlobalApiResponse } from '@/app/utils/globalsApiResponse';
 import { useAuth } from '@/app/hook/auth/useAuth';
+import { useRouter } from 'next/navigation';
 
 // Mendefinisikan tipe data User
 interface User {
@@ -30,7 +31,7 @@ export const authContext = createContext<AuthContextType>({
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
 	const [user, setUser] = useState<User | null>(null);
 	const [authenticated, setAuthenticated] = useState<boolean>(false);
-
+	const router = useRouter();
 	const checkAuth = async () => {
 		const response = await useAuth();
 		if (response.authenticated && response.email && response.role) {
@@ -49,6 +50,7 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
 		Cookies.remove('access-token');
 		setAuthenticated(false);
 		setUser(null);
+		router.refresh();
 	};
 
 	useEffect(() => {
