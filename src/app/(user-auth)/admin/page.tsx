a17c264a62e1c3e41093925ecf5bf5a8b8ba024f
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-16 13:28:18 +0700

    feat: remove unused imports and simplify Home component structure

--- src/app/(user-auth)/admin/page.tsx
+++ src/app/(user-auth)/admin/page.tsx
@@ -1,10 +1,7 @@
 'use client';
 import { authContext } from '@/app/provider/auth-provider/authProvider';
-import { colorPallete } from '@/app/utils/colorspallete';
-import { Typography } from '@mui/material';
 import Stack from '@mui/material/Stack';
 import { useContext } from 'react';
-import Section from './Section';
 
 export default function Home() {
 	const auth = useContext(authContext);
@@ -13,10 +10,6 @@ export default function Home() {
 		<Stack
 			sx={{
 				minHeight: '100vh',
-			}}>
-			<Stack sx={{ gridColumn: { xs: 'span 1', md: 'span 2' }, padding: 1 }}>
-				<Section />
-			</Stack>
-		</Stack>
+			}}></Stack>
 	);
 }
