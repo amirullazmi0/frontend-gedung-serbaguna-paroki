c2d43e6030d1358204646c1fd56b1ae861c8963e
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-16 13:32:24 +0700

    feat: refactor Home component to fetch and display building data using MapBuilding

--- src/app/(user-auth)/admin/map/page.tsx
+++ src/app/(user-auth)/admin/map/page.tsx
@@ -4,29 +4,27 @@ import { colorPallete } from '@/app/utils/colorspallete';
 import { Typography } from '@mui/material';
 import Stack from '@mui/material/Stack';
 import { useContext } from 'react';
+import MapBuilding from './components/MapBuilding';
+import useQueryApiRequest from '@/app/hook/useQueryApiRequest';
+import { GlobalApiResponse } from '@/app/utils/globalsApiResponse';
+import { BuildingItemType } from '@/app/DTO/building';
 
 export default function Home() {
 	const auth = useContext(authContext);
-	console.log('auth', auth);
+	const { data: buildingResponse } = useQueryApiRequest<GlobalApiResponse<BuildingItemType[]>>({
+		key: 'get-admin-building',
+		withAuth: true,
+	});
+
+	const buildings = buildingResponse?.data;
 
 	return (
 		<Stack
 			sx={{
-				display: 'grid',
-				gridTemplateColumns: {
-					xs: 'repeat(1, 1fr)',
-					md: 'repeat(3, 1fr)',
-				},
 				minHeight: '100svh',
 			}}>
-			<Stack sx={{ gridColumn: { xs: 'span 1', md: 'span 2' } }}></Stack>
-			<Stack
-				sx={{
-					bgcolor: colorPallete['low-blue'],
-					padding: 1,
-				}}>
-				<Typography sx={{ color: colorPallete.white }}>{auth.user?.email}</Typography>
-				<Typography sx={{ color: colorPallete.white }}>{auth.user?.role}</Typography>
+			<Stack>
+				<MapBuilding building={buildings} />
 			</Stack>
 		</Stack>
 	);
