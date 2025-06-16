270a444dfed4d87a13291435efdbe3d809c2ed99
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-16 13:32:37 +0700

    feat: create Home component to display building cards using Stack layout

---
+++ src/app/building/page.tsx
@@ -0,0 +1,10 @@
+import Stack from '@mui/material/Stack';
+import BuildingCard from './components/BuildingCard';
+
+export default function Home() {
+	return (
+		<Stack>
+			<BuildingCard />
+		</Stack>
+	);
+}
