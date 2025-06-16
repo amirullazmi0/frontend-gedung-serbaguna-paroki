6666db58ebbad16fbdcfc64a652917b4e16f90ca
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-16 13:33:35 +0700

    refactor: simplify Section component layout by removing grid styling

--- src/app/components/Section.tsx
+++ src/app/components/Section.tsx
@@ -6,25 +6,8 @@ import DashboardMap from './DashboardMap';
 
 const Section = () => {
 	return (
-		<Stack
-			sx={{
-				display: 'grid',
-				gridTemplateColumns: {
-					xs: 'repeat(1, 1fr)',
-					md: 'repeat(4, 1fr)',
-				},
-			}}>
-			<Stack
-				gridColumn={{
-					xs: 'span 1',
-					md: 'span 4',
-				}}
-				sx={{
-					height: '100%', // Make this Stack fill available height
-					overflowY: 'auto', // allow scrolling if needed
-				}}>
-				<DashboardMap />
-			</Stack>
+		<Stack>
+			<DashboardMap />
 		</Stack>
 	);
 };
