974fcd0567b010f260f3991e59f9dfc6becd6fb4
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-17 15:03:51 +0700

    feat: simplify Home component by removing unused auth context and layout elements

--- src/app/(user-auth)/user/[building-id]/rent/page.tsx
+++ src/app/(user-auth)/user/[building-id]/rent/page.tsx
@@ -4,30 +4,17 @@ import { colorPallete } from '@/app/utils/colorspallete';
 import { Typography } from '@mui/material';
 import Stack from '@mui/material/Stack';
 import { useContext } from 'react';
+import FormRentBuilding from './components/FormRentBuilding';
 
 export default function Home() {
-	const auth = useContext(authContext);
-	console.log('auth', auth);
-
 	return (
 		<Stack
 			sx={{
 				display: 'grid',
-				gridTemplateColumns: {
-					xs: 'repeat(1, 1fr)',
-					md: 'repeat(3, 1fr)',
-				},
+
 				minHeight: '100svh',
 			}}>
-			<Stack sx={{ gridColumn: { xs: 'span 1', md: 'span 2' } }}></Stack>
-			<Stack
-				sx={{
-					bgcolor: colorPallete['low-blue'],
-					padding: 1,
-				}}>
-				<Typography sx={{ color: colorPallete.white }}>{auth.user?.email}</Typography>
-				<Typography sx={{ color: colorPallete.white }}>{auth.user?.role}</Typography>
-			</Stack>
+			<FormRentBuilding />
 		</Stack>
 	);
 }
