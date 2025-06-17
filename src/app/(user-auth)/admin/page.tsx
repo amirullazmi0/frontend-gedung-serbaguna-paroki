8a6a4b16ef4a7c6a9a49dae3f9bdc0afe5667065
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-17 15:03:35 +0700

    feat: integrate RentBuildingTable component into admin page layout

--- src/app/(user-auth)/admin/page.tsx
+++ src/app/(user-auth)/admin/page.tsx
@@ -2,6 +2,7 @@
 import { authContext } from '@/app/provider/auth-provider/authProvider';
 import Stack from '@mui/material/Stack';
 import { useContext } from 'react';
+import RentBuildingTable from './components/RentBuildingTable';
 
 export default function Home() {
 	const auth = useContext(authContext);
@@ -10,6 +11,8 @@ export default function Home() {
 		<Stack
 			sx={{
 				minHeight: '100vh',
-			}}></Stack>
+			}}>
+			<RentBuildingTable />
+		</Stack>
 	);
 }
