8b95c0a02f677828da6f78f28423f9c1203e0ad8
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-17 15:04:02 +0700

    feat: add support document requirement schema to rent building configuration

--- src/app/(user-auth)/user/[building-id]/rent/components/RentBuildingConfig.ts
+++ src/app/(user-auth)/user/[building-id]/rent/components/RentBuildingConfig.ts
@@ -1,5 +1,23 @@
 import * as yup from "yup";
+import dayjs, { Dayjs } from 'dayjs';
+
+export const supportDocumentRequirementSchema = yup.object({
+  supportDocumentId: yup.string().required('ID wajib diisi'),
+  documentUrl: yup.string().url('Format URL tidak valid').required('File wajib diupload'),
+})
 
 export const rentBuildingConfigSchema = yup.object({
-
-})
+  buildingId: yup.string().required('ID wajib diisi'),
+  startDate: yup
+    .mixed<Dayjs>()
+    .required('Tanggal mulai wajib diisi')
+    .test('is-dayjs', 'Format tanggal tidak valid', value => dayjs.isDayjs(value)),
+  endDate: yup
+    .mixed<Dayjs>()
+    .required('Tanggal selesai wajib diisi')
+    .test('is-dayjs', 'Format tanggal tidak valid', value => dayjs.isDayjs(value)),
+  supportDocumentRequirements: yup
+    .array()
+    .of(supportDocumentRequirementSchema)
+    .default([]),
+});
