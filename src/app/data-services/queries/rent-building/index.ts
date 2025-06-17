33b94303e743242a117a947786fbbb08bf3a0653
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-18 02:56:02 +0700

    feat: add 'get-rent-building-by-building' query to RentBuildingQueriesKeys

--- src/app/data-services/queries/rent-building/index.ts
+++ src/app/data-services/queries/rent-building/index.ts
@@ -1,8 +1,12 @@
 import { QueriesDataService } from '../../types';
 
-export type RentBuildingQueriesKeys = 'get-rent-building-by-user' | 'get-rent-building-by-admin';
+export type RentBuildingQueriesKeys =
+  'get-rent-building-by-user'
+  | 'get-rent-building-by-admin'
+  | 'get-rent-building-by-building'
 
 export const rentBuildingQueries: QueriesDataService<RentBuildingQueriesKeys> = {
   'get-rent-building-by-user': 'rent-building/user',
   'get-rent-building-by-admin': 'rent-building/admin',
+  'get-rent-building-by-building': 'rent-building/building',
 };
