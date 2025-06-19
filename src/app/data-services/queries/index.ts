3fe9fdebb8235d34d9a0008589712b88a0a0a9ea
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-20 00:31:47 +0700

    feat: add user queries to allQueries for comprehensive data handling

--- src/app/data-services/queries/index.ts
+++ src/app/data-services/queries/index.ts
@@ -1,10 +1,12 @@
 import { QueriesDataService } from "../types";
 import { buildingQueries, BuildingQueriesKeys } from "./building";
 import { rentBuildingQueries, RentBuildingQueriesKeys } from "./rent-building";
+import { userQueries, UserQueriesKeys } from "./user";
 
-export type AllQueriesKeys = BuildingQueriesKeys | RentBuildingQueriesKeys
+export type AllQueriesKeys = BuildingQueriesKeys | RentBuildingQueriesKeys | UserQueriesKeys
 
 export const allQueries: QueriesDataService<AllQueriesKeys> = {
   ...buildingQueries,
-  ...rentBuildingQueries
+  ...rentBuildingQueries,
+  ...userQueries
 }
